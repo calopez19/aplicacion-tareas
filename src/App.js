136db3ba03d8c89444a5b40ee1d2f3e0fedae088
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-11-18 19:16:11 -0300

    estilo de la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Logo from './imagenes/logo_monster.png'
 
 function App() {
   return (
-    <div className="App">
-      hola mundo
+    <div className="Aplicacion-tareas">
+      <div className="contenedor-logo">
+        <img src={Logo} alt="logo de monster" className='logo'/>
+      </div>
     </div>
   );
 }
